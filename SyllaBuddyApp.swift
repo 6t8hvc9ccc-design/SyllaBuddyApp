5530c1fd211a6a7d5f770eaef6082093a925e45a
Danamk <6t8hvc9ccc@privaterelay.appleid.com>
2026-09-20 16:05:47 +0300

    Update SyllaBuddyApp.swift

--- SyllaBuddyApp.swift
+++ SyllaBuddyApp.swift
@@ -5,3 +5,21 @@
 //  Created by DMK on 20/09/2026.
 //
 
+import SwiftUI
+ 
+@main
+struct SyllaBuddyApp: App {
+    @StateObject private var store = Store()
+ 
+    init() {
+        NotificationManager.shared.requestAuthorization()
+    }
+ 
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+                .environmentObject(store)
+        }
+    }
+}
+ 
